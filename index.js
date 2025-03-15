a9ed5c7e6166db4be19c7424e4d6edf1e398ee00
Anamika <anamikahajipur2000@gmail.com>
2025-03-15 21:55:14 +0530

    install cors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 const userRoutes = require('./Routes/UserRoutes')
+const cors = require("cors");
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
+
+app.use(cors({
+  origin: "*",  // Allow all origins (change this to specific domains in production)
+  methods: ["GET", "POST","PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 const MongoURI = process.env.MONGOURL
 const PORT = process.env.PORT || 3000
